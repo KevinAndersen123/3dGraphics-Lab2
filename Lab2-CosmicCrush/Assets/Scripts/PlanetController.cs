8ff2a9c5b56ff710fc7f04dd358d8c55a60ccd04
Krystian Sarowski <c00225629@itcarlow.ie>
2019-09-30 16:35:37 +0100

    Planets pull in player based on mass diff

--- Lab2-CosmicCrush/Assets/Scripts/PlanetController.cs
+++ Lab2-CosmicCrush/Assets/Scripts/PlanetController.cs
@@ -12,10 +12,46 @@ public class PlanetController : MonoBehaviour
 
     public Rigidbody2D rb2d;
 
+    public GameObject player;
+
+    bool isPlayerAttracted;
+
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         velocity = new Vector3(Random.Range(-100, 100), Random.Range(-100, 100));
         rb2d.AddForce(velocity.normalized * speed);
+    }
+
+    void FixedUpdate()
+    {
+        if(isPlayerAttracted)
+        {
+            if(rb2d.mass > player.GetComponent<Rigidbody2D>().mass)
+            {
+                Vector3 VectorToPlanet = (transform.position - player.transform.position).normalized;
+                VectorToPlanet *= (rb2d.mass - player.GetComponent<Rigidbody2D>().mass) * 5;
+
+                player.GetComponent<Rigidbody2D>().AddForce(VectorToPlanet);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D t_colliderInfo)
+    {
+        if (t_colliderInfo.gameObject == player)
+        {
+            isPlayerAttracted = true;
+            Debug.Log("Player is attracted");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D t_colliderInfo)
+    {
+        if (t_colliderInfo.gameObject == player)
+        {
+            isPlayerAttracted = false;
+        }
     }
 }
