691f25f5111abbe6e253c16674d0036f7b77083b
Krystian Sarowski <c00225629@itcarlow.ie>
2019-10-02 11:49:43 +0100

    Added the abilty to restart the game and quit it

---
+++ Lab2-CosmicCrush/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+//@Author Krystian Sarowski
+
+public class GameManager : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+
+    //Restarts the current level.
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //Closes down the application.
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
